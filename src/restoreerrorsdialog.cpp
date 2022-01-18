06d8894a100dbb71b55962094ef06cd00586e91a
aryserg <aryserg3@gmail.com>
2022-01-18 18:21:10 +0300

    Edit

--- src/restoreerrorsdialog.cpp
+++ src/restoreerrorsdialog.cpp
@@ -17,13 +17,14 @@ RestoreErrorsDialog::RestoreErrorsDialog(QWidget *parent, QStringList items) :
 	movie.setSpeed(25);
 	movie.setCacheMode(QMovie::CacheAll);
 	int height_label = 4 * m_ui->label->height();
-	float Kos(1.0f);
+	double Kos = 1.0;
+
 #if defined(__LINUX__)
 	height_label = 20;
 #endif
 
 #if defined(__APPLE__)
-	Kos = 1.25f;
+	Kos = 1.25;
 #endif
 
 	QFontMetrics font_metrics(m_ui->textBrowser->font());
@@ -31,7 +32,7 @@ RestoreErrorsDialog::RestoreErrorsDialog(QWidget *parent, QStringList items) :
 
 	// Get the height by multiplying number of lines by font height, Maybe add to this a bit for a slight margin?
 	int height = font_height * 8;
-	float scale_font = Kos * font_height / 15;
+	double scale_font = Kos * font_height / 15;
 
 	// Set the height to the text broswer
 	m_ui->textBrowser->setMinimumHeight(height);
@@ -43,8 +44,6 @@ RestoreErrorsDialog::RestoreErrorsDialog(QWidget *parent, QStringList items) :
 
 	// Get the height by multiplying number of lines by font height, Maybe add to this a bit for a slight margin?
 	int heightlist = scale_font*font_heightlist * 5;
-
-	//int height_label = 4*m_ui->label->height();
 
 	int widgheigt = height + height_label + heightlist + (int)(scale_font * 50) + m_ui->buttonBox->height();
 	this->resize((int)(scale_font*this->width()), widgheigt);
