75e51672073a88869aaddaae778d10cc85a01374
Daniel Sorokin <sorokin_da@physlab.ru>
2022-08-23 17:43:45 +0300

    Added temporary dir for easier cleanup

--- profiles.sh
+++ profiles.sh
@@ -1,16 +1,18 @@
 H_DIR=$XIMC_DIR/c-profiles/STANDA
 PY_DIR=$XIMC_DIR/python-profiles/STANDA
+TMP_DIR=$ARCHIVE_DIR/tmp
+mkdir $TMP_DIR
 
 for path in ${H_DIR}/*.h; do
 	filename="${path##*/}"
 	profname="${filename%.*}"
-	tar -rvf ${ARCHIVE_DIR}/${profname}.tar -C $H_DIR $filename
+	tar -rvf ${TMP_DIR}/${profname}.tar -C $H_DIR $filename
 done	
 
 for path in ${PY_DIR}/*.py; do
 	filename="${path##*/}"
 	profname="${filename%.*}"
-	tar -rvf ${ARCHIVE_DIR}/${profname}.tar -C $PY_DIR $filename
+	tar -rvf ${TMP_DIR}/${profname}.tar -C $PY_DIR $filename
 done	
 
 for path in ${CFG_DIR}/*.cfg; do
@@ -18,16 +20,17 @@ for path in ${CFG_DIR}/*.cfg; do
 	profname="${filename%.*}"
 	if [ -e $path ]
 	then
-		tar -rvf ${ARCHIVE_DIR}/${profname}.tar -C $CFG_DIR $filename
+		tar -rvf ${TMP_DIR}/${profname}.tar -C $CFG_DIR $filename
 	else
 		echo $path
 	fi
 done
 
-for path in ${ARCHIVE_DIR}/*.tar; do
+for path in ${TMP_DIR}/*.tar; do
 	gzip $path
-	tar -rvf profile-archives.tar ${path}.gz
+	tar -rvf profile-archives.tar -C $TMP_DIR ${path}.gz
 done
 
 gzip profile-archives.tar
-rm -rvf $ARCHIVE_DIR
+mv profile-archives.tar.gz $ARCHIVE_DIR
+rm -rvf $TMP_DIR
