dae1609c68bcb731d3af673c0a5898771e8ea02d
Daniel Sorokin <sorokin_da@physlab.ru>
2022-08-23 16:47:29 +0300

    Switched from zip to tar

--- profiles.sh
+++ profiles.sh
@@ -4,20 +4,24 @@ PY_DIR=$XIMC_DIR/python-profiles/STANDA
 for path in ${H_DIR}/*.h; do
 	filename="${path##*/}"
 	profname="${filename%.*}"
-	zip -j ${ARCHIVE_DIR}/${profname}.zip $path
+	tar -rvf ${ARCHIVE_DIR}/${profname}.tar -C $H_DIR $filename
 done	
 
 for path in ${PY_DIR}/*.py; do
 	filename="${path##*/}"
 	profname="${filename%.*}"
-	zip -j ${ARCHIVE_DIR}/${profname}.zip $path
+	tar -rvf ${ARCHIVE_DIR}/${profname}.tar -C $PY_DIR $filename
 done	
 
 for path in ${CFG_DIR}/*.cfg; do
 	filename="${path##*/}"
 	profname="${filename%.*}"
-	zip -j ${ARCHIVE_DIR}/${profname}.zip $path
+	tar -rvf ${ARCHIVE_DIR}/${profname}.tar -C $CFG_DIR $filename
 done
 
-zip -r profile-archives.zip $ARCHIVE_DIR
-rm -rf $ARCHIVE_DIR
+for path in ${ARCHIVE_DIR}/*.tar; do
+	gzip $path
+	tar -rvf profile-archives.tar ${path}.gz
+done
+gzip profile-archives.tar
+#rm -rvf $ARCHIVE_DIR
